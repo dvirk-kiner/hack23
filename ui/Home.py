e01c468de410a0d202fb69d1f25dcb2c77f44d6d
NoaAbecasis <noaabecasis322@gmail.com>
2023-09-17 05:15:11 +0300

    name update

--- ui/Home.py
+++ ui/Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.set_page_config(page_title="Hello", page_icon="🚛")
-st.markdown("# Welcome to Hack Zurich!")
+st.markdown("# Welcome to Carbon Saver Brothers!")
 
 st.markdown(
     """
